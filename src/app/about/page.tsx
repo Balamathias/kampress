55df3a299d0ead7c84e1f09883ebaa65daa6ddcf
Ladalo <balaladalo@gmail.com>
2025-11-12 13:43:16 +0100

    Brief UI Modification

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,12 +3,14 @@ import WhoAreWe from "@/components/about/WhoAreWe";
 import MissionVisionCards from "@/components/about/MissionVisionCards";
 import StrategyValues from "@/components/about/StrategyValues";
 import SimpleSteps from "@/components/about/SimpleSteps";
+import GetStartedFormSection from "@/components/forms/GetStartedFormSection";
 
 export default function AboutPage() {
   return (
     <main className="min-h-screen">
       <AboutHero />
       <WhoAreWe />
+      <GetStartedFormSection />
       <MissionVisionCards />
       <StrategyValues />
       <SimpleSteps />
