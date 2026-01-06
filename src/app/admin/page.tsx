2a69994cc0cac0fb7117badb92a9b108d9d3a8f5
Ladalo <balaladalo@gmail.com>
2026-01-06 09:20:09 +0100

    Fix SSR redirect error for admin and inbox pages

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,17 +1,17 @@
 "use client"
 
-import { useEffect } from 'react';
 
-const AdminPage = () => {
-  useEffect(() => {
-    window.location.href = 'https://da22.host-ww.net:2222/evo/login';
-  }, []);
+import { useRouter } from 'next/navigation';
+import React from 'react'
+
+const page = () => {
+    const router = useRouter();
+    return router?.push('https://da22.host-ww.net:2222/evo/login');
+
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <p>Redirecting to admin panel...</p>
-    </div>
-  );
+    <div>page</div>
+  )
 }
 
-export default AdminPage
+export default page
--- src/app/inbox/page.tsx
+++ src/app/inbox/page.tsx
@@ -1,17 +1,17 @@
 "use client"
 
-import { useEffect } from 'react';
 
-const InboxPage = () => {
-  useEffect(() => {
-    window.location.href = 'https://da22.host-ww.net/roundcube/';
-  }, []);
+import { useRouter } from 'next/navigation';
+import React from 'react'
+
+const page = () => {
+    const router = useRouter();
+    return router?.push('https://da22.host-ww.net/roundcube/');
+
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <p>Redirecting to inbox...</p>
-    </div>
-  );
+    <div>page</div>
+  )
 }
 
-export default InboxPage
+export default page
